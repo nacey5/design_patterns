8545baf9c47b39ba6c3ddd97edf36009a16ce582
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-08-31 00:37:08 +0800

    Create TShirt.java

---
+++ design_pattern/detail_wear/TShirt.java
@@ -0,0 +1,18 @@
+/**
+ * @NAME: Tshirts
+ * @USER: DaHuangGO
+ * @DATE: 2022/8/31
+ * @TIME: 0:12
+ * @YEAR: 2022
+ * @MONTH: 08
+ * @DAY: 31
+ */
+public class TShirt extends Finery {
+
+    @Override
+    public void show() {
+        System.out.print("大T恤\t");
+        super.show();
+    }
+
+}
