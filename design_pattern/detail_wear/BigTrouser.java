53aa9a851aecfb8bd152eb501bac453d94ea97a8
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-08-31 00:36:39 +0800

    Create BigTrouser.java

---
+++ design_pattern/detail_wear/BigTrouser.java
@@ -0,0 +1,16 @@
+/**
+ * @NAME: BigTrouser
+ * @USER: DaHuangGO
+ * @DATE: 2022/8/31
+ * @TIME: 0:14
+ * @YEAR: 2022
+ * @MONTH: 08
+ * @DAY: 31
+ */
+public class BigTrouser extends Finery {
+    @Override
+    public void show() {
+        System.out.print("垮裤\t");
+        super.show();
+    }
+}
