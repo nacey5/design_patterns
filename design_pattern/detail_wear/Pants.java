f36c35cef5f8f416c23610bd4cacd87747e45843
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-08-31 00:36:53 +0800

    Create Pants.java

---
+++ design_pattern/detail_wear/Pants.java
@@ -0,0 +1,16 @@
+/**
+ * @NAME: Pants
+ * @USER: DaHuangGO
+ * @DATE: 2022/8/31
+ * @TIME: 0:14
+ * @YEAR: 2022
+ * @MONTH: 08
+ * @DAY: 31
+ */
+public class Pants extends Finery {
+    @Override
+    public void show() {
+        System.out.print("大长裤\t");
+        super.show();
+    }
+}
