9c253151aecb170dd4c5cf89bb4c1efb7d42a2a3
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-01 00:56:02 +0800

    Create GiveGift.java

---
+++ proxy_pattern/instance/GiveGift.java
@@ -0,0 +1,14 @@
+/**
+ * @NAME: GiveGift
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/1
+ * @TIME: 0:45
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 01
+ */
+public interface GiveGift {
+    void giveDolls();
+    void giveFlows();
+    void giveChocolate();
+}
