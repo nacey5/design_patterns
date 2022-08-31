48c33562ef505b25354fe6bb5117641815ac8a4c
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-01 00:54:32 +0800

    Create Client.java

---
+++ proxy_pattern/Client.java
@@ -0,0 +1,15 @@
+/**
+ * @NAME: Client
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/1
+ * @TIME: 0:42
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 01
+ */
+public class Client {
+    public static void main(String[] args) {
+        Proxy proxy=new Proxy();
+        proxy.request();
+    }
+}
