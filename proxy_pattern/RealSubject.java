7a6bf54e7bb55b87d9bc979006a69ca6a46c8bfe
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-01 00:55:03 +0800

    Create RealSubject.java

---
+++ proxy_pattern/RealSubject.java
@@ -0,0 +1,16 @@
+/**
+ * @NAME: RealSubject
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/1
+ * @TIME: 0:40
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 01
+ */
+public class RealSubject extends Subject{
+    @Override
+    public void request() {
+        System.out.println("我在干好事！！！！");
+        System.out.println("真实的请求");
+    }
+}
