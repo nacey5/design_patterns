8b0ca4142958a84b58da0edaa3c5c20a96ca4d59
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-01 00:56:51 +0800

    Create SchoolGirl.java

---
+++ proxy_pattern/instance/SchoolGirl.java
@@ -0,0 +1,24 @@
+/**
+ * @NAME: SchoolGirl
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/1
+ * @TIME: 0:47
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 01
+ */
+public class SchoolGirl {
+    private String name;
+
+    public SchoolGirl(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
