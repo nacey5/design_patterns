e295d61d2ff8498fc14f8172622a3bd724ea5745
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-01 00:55:17 +0800

    Create Subject.java

---
+++ proxy_pattern/Subject.java
@@ -0,0 +1,13 @@
+/**
+ * 是realSubject和Proxy的公用接口
+ * @NAME: Subject
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/1
+ * @TIME: 0:38
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 01
+ */
+abstract class Subject {
+    public abstract void request();
+}
