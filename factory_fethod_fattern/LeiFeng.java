9e9692086a63ab97f32f940edf15d8e4e291401c
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-02 23:05:58 +0800

    Create LeiFeng.java

---
+++ factory_fethod_fattern/LeiFeng.java
@@ -0,0 +1,23 @@
+/**
+ * 雷锋类/实体1
+ * @NAME: LeiFeng
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/2
+ * @TIME: 22:44
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 02
+ */
+public class LeiFeng {
+    public void sweep(){
+        System.out.println("扫地");
+    }
+
+    public void Wash(){
+        System.out.println("洗衣");
+    }
+
+    public void BuyRice(){
+        System.out.println("买米");
+    }
+}
