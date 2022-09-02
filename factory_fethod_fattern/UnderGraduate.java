0d29a39342380c57d751d3ef0b85bfdb2e6358ea
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-02 23:06:12 +0800

    Create UnderGraduate.java

---
+++ factory_fethod_fattern/UnderGraduate.java
@@ -0,0 +1,12 @@
+/**
+ * 大学生类/实体2
+ * @NAME: UnderGraduate
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/2
+ * @TIME: 22:45
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 02
+ */
+public class UnderGraduate extends LeiFeng{
+}
