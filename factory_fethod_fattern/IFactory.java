e0b3b6c2099c1a4d1bb805404626105489b75f1a
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-02 23:05:39 +0800

    Create IFactory.java

---
+++ factory_fethod_fattern/IFactory.java
@@ -0,0 +1,13 @@
+/**
+ * 抽象工厂
+ * @NAME: IFactory
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/2
+ * @TIME: 22:43
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 02
+ */
+public interface IFactory {
+   public LeiFeng CreateLeiFeng();
+}
