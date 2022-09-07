be21bee4e77e32c61636291b111f8b8ed3c6957f
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-07 20:08:50 +0800

    Create ConcreteClassB.java

---
+++ template_method_pattern/ConcreteClassB.java
@@ -0,0 +1,20 @@
+/**
+ * @NAME: ConcreteClassB
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/6
+ * @TIME: 22:27
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 06
+ */
+public class ConcreteClassB extends AbstractClass{
+    @Override
+    public void primitiveOperation1() {
+        System.out.println("具体类B的方法1的实现");
+    }
+
+    @Override
+    public void primitiveOperation2() {
+        System.out.println("具体类B的方法2的实现");
+    }
+}
