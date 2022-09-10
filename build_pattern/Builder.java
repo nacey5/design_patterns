76623aa433de8ff1cd0d2d18fc85ae7f8acfc486
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-10 20:26:48 +0800

    Create Builder.java

---
+++ build_pattern/Builder.java
@@ -0,0 +1,14 @@
+/**
+ * @NAME: Builder
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/10
+ * @TIME: 20:16
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 10
+ */
+public interface Builder {
+    public abstract void buildPartA();
+    public abstract void buildPartB();
+    public abstract Product getResult();
+}
