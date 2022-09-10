338733bef0df5a38b28d98d07654912500fb6fbc
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-10 20:27:59 +0800

    Create Director.java

---
+++ build_pattern/Director.java
@@ -0,0 +1,15 @@
+/**
+ * @NAME: Director
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/10
+ * @TIME: 20:20
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 10
+ */
+public class Director {
+    public void startConstruct(Builder builder){
+        builder.buildPartA();
+        builder.buildPartB();
+    }
+}
