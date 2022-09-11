5cd66c27ace989e240f55e53f5fc66e7e38f27d9
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-11 22:53:50 +0800

    Create Observer.java

---
+++ observer_pattern/Observer.java
@@ -0,0 +1,13 @@
+/**
+ * 抽象观察者
+ * @NAME: Observer
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/11
+ * @TIME: 22:24
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 11
+ */
+public abstract class Observer {
+    public abstract void update();
+}
