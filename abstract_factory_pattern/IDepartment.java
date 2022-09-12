52fdb9ed1ecee970bd269473b560e40ed24e3655
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-12 22:39:44 +0800

    Create IDepartment.java

---
+++ abstract_factory_pattern/IDepartment.java
@@ -0,0 +1,14 @@
+/**
+ * @NAME: IDepartment
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/12
+ * @TIME: 22:24
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 12
+ */
+public interface IDepartment {
+    void insert(Department department);
+
+    Department getDepartment(int id);
+}
