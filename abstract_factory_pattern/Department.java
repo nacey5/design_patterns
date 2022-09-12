5a53e0824c3f68ef90edb325ee27bd3fd02f45fe
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-12 22:39:30 +0800

    Create Department.java

---
+++ abstract_factory_pattern/Department.java
@@ -0,0 +1,29 @@
+/**
+ * @NAME: Department
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/12
+ * @TIME: 22:25
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 12
+ */
+public class Department {
+    private String name;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
