45c51e03f8cec55cc47e3163346641ab9dbca499
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-12 22:40:34 +0800

    Create User.java

---
+++ abstract_factory_pattern/User.java
@@ -0,0 +1,29 @@
+/**
+ * @NAME: User
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/12
+ * @TIME: 22:15
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 12
+ */
+public class User {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
