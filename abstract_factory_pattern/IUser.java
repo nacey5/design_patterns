3d78a93153960fd4fe75fe0093b4a1a4336cecbc
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-12 22:39:57 +0800

    Create IUser.java

---
+++ abstract_factory_pattern/IUser.java
@@ -0,0 +1,14 @@
+/**
+ * @NAME: IUser
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/12
+ * @TIME: 22:15
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 12
+ */
+public interface IUser {
+    void insert(User user);
+
+    User getUser(int id);
+}
