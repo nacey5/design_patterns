8628431e881fa5ee1179958df3e3f5cba790ab22
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-13 22:08:58 +0800

    Create State.java

---
+++ state_pattern/State.java
@@ -0,0 +1,12 @@
+/**
+ * @NAME: State
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/13
+ * @TIME: 19:39
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 13
+ */
+public abstract class State {
+    public abstract void handler(Context context);
+}
