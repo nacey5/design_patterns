86636e5f8fc81dd5c72dfa2c991ec775ad8bc58c
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-15 00:11:04 +0800

    Create Target.java

---
+++ adapter_pattern/Target.java
@@ -0,0 +1,17 @@
+/**
+ * 客户端所期待的接口
+ * @NAME: Target
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/14
+ * @TIME: 23:41
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 14
+ */
+public class Target {
+
+    public void request(){
+        System.out.println("普通请求");
+    }
+
+}
