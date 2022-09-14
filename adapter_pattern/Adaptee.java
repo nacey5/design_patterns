bca15ff1264a9f7924ab5110ae5b9822bccc16c8
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-15 00:10:26 +0800

    Create Adaptee.java

---
+++ adapter_pattern/Adaptee.java
@@ -0,0 +1,15 @@
+/**
+ * 需要适配的类
+ * @NAME: Adaptee
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/14
+ * @TIME: 23:42
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 14
+ */
+public class Adaptee {
+    public void SpecificRequest(){
+        System.out.println("特殊请求");
+    }
+}
