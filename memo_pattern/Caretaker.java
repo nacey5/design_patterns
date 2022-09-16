33420c96575634e85de65caa021fbed65c7fbc46
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-17 00:23:52 +0800

    Create Caretaker.java

---
+++ memo_pattern/Caretaker.java
@@ -0,0 +1,21 @@
+/**
+ * 管理类
+ * @NAME: Caretaker
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/17
+ * @TIME: 0:19
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 17
+ */
+public class Caretaker {
+    private Memento memento;
+
+    public Memento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(Memento memento) {
+        this.memento = memento;
+    }
+}
