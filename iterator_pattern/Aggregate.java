21da694f59824d7537af05a9404250e0e9824baa
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-09-28 20:34:37 +0800

    Create Aggregate.java

---
+++ iterator_pattern/Aggregate.java
@@ -0,0 +1,13 @@
+/**
+ * Aggregate聚集抽象类
+ * @NAME: Aggregate
+ * @USER: DaHuangGO
+ * @DATE: 2022/9/28
+ * @TIME: 20:15
+ * @YEAR: 2022
+ * @MONTH: 09
+ * @DAY: 28
+ */
+public abstract class Aggregate {
+    public abstract Iterator createIterator();
+}
