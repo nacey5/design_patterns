ad8578cb4c0d1c27d5b2e2c393c6322c17f73249
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-11-21 11:11:09 +0800

    Create Abstraction.java

---
+++ bridge_pattern/Abstraction.java
@@ -0,0 +1,11 @@
+public class Abstraction {
+    protected Implementor implementor;
+
+    public void setImplementor(Implementor implementor) {
+        this.implementor = implementor;
+    }
+
+    public void operation(){
+        implementor.operation();
+    }
+}
