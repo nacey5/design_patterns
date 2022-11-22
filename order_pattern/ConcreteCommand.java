52c06abe7762b0729d11643b75b9ac513d9343b9
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-11-22 11:19:29 +0800

    Create ConcreteCommand.java

---
+++ order_pattern/ConcreteCommand.java
@@ -0,0 +1,11 @@
+public class ConcreteCommand extends Command{
+
+    public ConcreteCommand(Receiver receiver) {
+        super(receiver);
+    }
+
+    @Override
+    public void execute() {
+        receiver.action();
+    }
+}
