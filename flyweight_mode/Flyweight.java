6fd3bfe9d16e0ea2e2bae38f2a2a5cb1036a5510
DaHuangGO <85286598+nacey5@users.noreply.github.com>
2022-12-14 16:22:59 +0800

    Create Flyweight.java

---
+++ flyweight_mode/Flyweight.java
@@ -0,0 +1,13 @@
+/**
+ * @NAME: Flyweight
+ * @USER: DaHuangGO
+ * @DATE: 2022/12/14
+ * @TIME: 16:05
+ * @YEAR: 2022
+ * @MONTH: 12
+ * @DAY: 14
+ * 享元的超类
+ */
+public abstract class Flyweight {
+    public abstract void operation(int extrinsicstate);
+}
